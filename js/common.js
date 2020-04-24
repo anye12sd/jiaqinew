af1eb48da2dc43c89bc8a51f65cb6bf2c26be1b4
anye12sd <541890086@qq.com>
2020-04-24 17:13:26 +0800

    8th

--- js/common.js
+++ js/common.js
@@ -1,6 +1,73 @@
-$(function(){
-    
-})
+if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function(callback/*, thisArg*/) {
+        var T, k;
+        if (this == null) {
+            throw new TypeError('this is null or not defined');
+        }
+        var O = Object(this);
+        var len = O.length >>> 0;
+        if (typeof callback !== 'function') {
+            throw new TypeError(callback + ' is not a function');
+        }
+
+        if (arguments.length > 1) {
+            T = arguments[1];
+        }
+
+        k = 0;
+
+        while (k < len) {
+            var kValue;
+
+            if (k in O) {
+
+                kValue = O[k];
+
+                callback.call(T, kValue, k, O);
+            }
+
+            k++;
+        }
+
+    };
+}
+
+if (!Array.prototype.filter)
+{
+    Array.prototype.filter = function(fun /*, thisp */)
+    {
+        "use strict";
+
+        if (this === void 0 || this === null)
+            throw new TypeError();
+
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (typeof fun !== "function")
+            throw new TypeError();
+
+        var res = [];
+        var thisp = arguments[1];
+        for (var i = 0; i < len; i++)
+        {
+            if (i in t)
+            {
+                var val = t[i]; // in case fun mutates this
+                if (fun.call(thisp, val, i, t))
+                    res.push(val);
+            }
+        }
+
+        return res;
+    };
+}
+
+if (!String.prototype.includes) {
+    String.prototype.includes = function() {
+        'use strict';
+        return String.prototype.indexOf.apply(this, arguments)!== -1;
+    };
+}
 
 $(function(){
     $(".site-nav li").mouseenter(function(){
